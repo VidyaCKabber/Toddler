f793e4aeb554b47aa3dc29b8d04f1223cf1b8254
Vidya Channabasava Kabber <vidyack99@gmail.com>
2020-01-31 18:51:26 +0530

    configuring with sqlite

--- App.js
+++ App.js
@@ -1,10 +1,40 @@
 //import liraries
-import React, { Component } from 'react';
+import React, { Component,useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { MyComponent } from './src/screens/Todo';
 
+var SQLite = require('react-native-sqlite-storage');
+var db = SQLite.openDatabase({
+  name: 'test.sqlite3',
+  createFromLocation: '~example.sqlite3',
+});
+
 // create a component
 export default function App(){
+
+  useEffect(() =>{
+      
+      return new Promise(() => {
+        db.transaction((tx) => {
+            tx.executeSql('INSERT INTO `first`(`name`) VALUES (?)', ['hima'], (tx, results) => {
+              console.log("results.rowsAffected",results.rowsAffected);  
+              if (results.rowsAffected > 0) {
+                    console.log('Created successfully!');
+                } else {
+                  console.log('failed!');
+                }
+               
+            }, (error) => {
+              console.log('failed!',error);
+            });
+        })
+    })
+
+    
+    })
+
+        
+
   return (
     <MyComponent/>
   );
--- android/app/src/main/java/com/test/MainApplication.java
+++ android/app/src/main/java/com/test/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import org.pgsqlite.SQLitePluginPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          new SQLitePluginPackage();  // register SQLite Plugin here
           return packages;
         }
 
