07aa34922ee55cd2f6868c859510520a66e6452a
Vidya Channabasava Kabber <vidyack99@gmail.com>
2020-02-03 15:44:15 +0530

    First level is completed

--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -13,22 +13,35 @@ import TodoList from './TodoList';
 
 var SQLite = require('react-native-sqlite-storage');
 var db = SQLite.openDatabase({
-  name: 'test.sqlite3',
-  createFromLocation: '~example.sqlite3',
+  name: 'test2.sqlite3',
+  createFromLocation: '~todo.sqlite3',
 });
+
+//get the date in data/month/year format
+var date = new Date();
+today =
+  date.getDate() +
+  '/' +
+  parseInt(date.getMonth() + 1) +
+  '/' +
+  date.getFullYear();
+
 // create a component
 export function MyComponent() {
   const [value, setValue] = useState('');
   const [todos, setTodos] = useState([]);
+  const [tasDonedOn, setTasDonedOn] = useState(today);
 
-  const addToto = () => {
+  const createTodo = () => {
     return new Promise(() => {
       db.transaction(tx => {
-        const squery = 'INSERT INTO `first` (`name`) VALUES (?)';
+        const squery =
+          'INSERT INTO `mytask` (`id`,`name`,`created_on`,`completed_on`) VALUES (?,?,?,?)';
         tx.executeSql(
           squery,
-          [value],
+          [Date.now(), value, today, ''],
           (tx, results) => {
+            console.log('Results', results.rowsAffected);
             if (results.rowsAffected > 0) {
               setValue('');
               setTodos(todos => [
@@ -50,7 +63,7 @@ export function MyComponent() {
 
   function getAllTasks() {
     db.transaction(tx => {
-      const squery = 'SELECT * FROM `first`;';
+      const squery = 'SELECT * FROM `mytask`;';
       tx.executeSql(squery, [], (tx, results) => {
         var len = results.rows.length;
         /**make todos array empty on each load*/
@@ -63,7 +76,7 @@ export function MyComponent() {
               {
                 text: results.rows.item(i).name,
                 key: Date.now(),
-                checked: false,
+                checked: results.rows.item(i).completed_on == ''? false : true /** check the completed tasks */
               },
             ]);
           }
@@ -76,31 +89,57 @@ export function MyComponent() {
     getAllTasks();
   }, [db]);
 
-  
-  const checkTodo = id => {
+  const completeTodo = (task_name, isCompleted) => {
+    
     setTodos(
       todos.map(todo => {
-        if (todo.key === id) todo.checked = !todo.checked;
+        if (todo.text === task_name) todo.checked = !todo.checked;
         return todo;
       }),
     );
+
+    return new Promise(() => {
+      //if tasks is completed then update the completed date
+      if (!isCompleted) {
+        setTasDonedOn(' ');
+      } else {
+        setTasDonedOn(today);
+      }
+     
+      db.transaction(tx => {
+        const squery = 'UPDATE `mytask` SET `completed_on`=?  WHERE `name`=?;';
+        tx.executeSql(
+          squery,
+          [tasDonedOn, task_name],
+          (tx, results) => {
+            console.log('Results', results.rowsAffected);
+            if (results.rowsAffected > 0) {
+              console.log('Successfully Updated');
+            }
+          },
+          error => {
+            console.log('update failed!', error);
+          },
+        );
+      });
+    });
   };
 
-  /**Delete the task using task name */
+  /**Delete the task using task_name */
   const deleteTodo = task_name => {
     return new Promise(() => {
       db.transaction(tx => {
-        const squery = 'DELETE FROM `first` WHERE `name`=?;';
+        const squery = 'DELETE FROM `mytask` WHERE `name`=?;';
         tx.executeSql(
           squery,
           [task_name],
           (tx, results) => {
             if (results.rowsAffected > 0) {
-              
-              setTodos(todos.filter(todos => todos !== task_name));/**Remove deleted item from the array */
-              
-              getAllTasks();/**Reload all tasks */
-              console.log('Deleted successfully!');
+              /**Remove deleted item from the array */
+              setTodos(todos.filter(todos => todos !== task_name));
+
+              getAllTasks(); /**Reload all tasks */
+              console.log('Deleted task is : ', task_name);
             } else {
               console.log('failed!');
             }
@@ -125,7 +164,7 @@ export function MyComponent() {
           value={value}
           onChangeText={value => setValue(value)}
         />
-        <TouchableOpacity onPress={() => addToto()}>
+        <TouchableOpacity onPress={() => createTodo()}>
           <Icon name="plus" size={30} color="blue" style={{marginLeft: 15}} />
         </TouchableOpacity>
       </View>
@@ -136,13 +175,13 @@ export function MyComponent() {
               text={item.text}
               key={item.key}
               checked={item.checked}
-              setChecked={() => checkTodo(item.key)}
+              setChecked={() => completeTodo(item.text, item.checked)}
               deleteTodo={() => deleteTodo(item.text)}
             />
           ))
         ) : (
           <View style={styles.noTask}>
-              <Text style={styles.noTaskTitle}> No tasks planned for today</Text>
+            <Text style={styles.noTaskTitle}> No tasks planned for today</Text>
           </View>
         )}
       </ScrollView>
@@ -162,7 +201,7 @@ const styles = StyleSheet.create({
     marginTop: '15%',
     fontSize: 25,
     color: 'blue',
-    fontWeight:'bold',
+    fontWeight: 'bold',
     paddingBottom: 10,
   },
   textInputContainer: {
@@ -180,12 +219,12 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     minHeight: '3%',
   },
-  noTask : {
-    marginTop:'50%',
-    alignItems:'center',
+  noTask: {
+    marginTop: '50%',
+    alignItems: 'center',
   },
-  noTaskTitle : {
-    fontSize:20,
-    fontWeight:'bold',
-  }
+  noTaskTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
